63b89921c32ff569493b2b47c1c60fc83fd9a78d
Erick Matsen <ematsen@gmail.com>
2010-12-09 06:21:35 -0800

    added template file

--- jsonrun.py
+++ jsonrun.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# possible TODOs:
+# - should we make it possible to specify a relative path for template_file?
+
 import sys, os, collections, string, argparse, re, subprocess, traceback
 import shmem 
 
@@ -17,6 +20,9 @@ SRUN = False                     # Whether or not to use srun, default is False 
 SRUN_COMMAND = 'srun -p emhigh ' # Specify srun command with any options such as which partition.
 DRYRUN = False                   # Run in dryrun mode, default is False.
 
+# what is used by default to run template files
+TEMPLATEFILE_RUN_CMD = 'source '
+
 
 def invoke(max_procs, json_files):
     """
@@ -26,6 +32,17 @@ def invoke(max_procs, json_files):
     pool.map(worker, json_files)
     #map(lambda f: worker(f, 1), json_files)
 
+# NOTE: can we avoid doing this by passing a flag to open to make it barf?
+def assert_file_exists(path):
+    if not os.path.isfile(path):
+	raise IOError("path "+path+" is not a file")
+
+# use d to do template substitution on each line of in_file and write the output to out_fobj
+def template_subs_file(in_file, out_fobj, d):
+    assert_file_exists(in_file)
+    with open(in_file, 'r') as in_fobj:
+	for line in in_fobj:
+	    out_fobj.write(string.Template(line).substitute(d))
 
 def worker(json_file):
     """
@@ -45,6 +62,12 @@ def worker(json_file):
 
     # cd into the directory containing the json file.
     os.chdir(json_directory)
+
+    # if a template file is being used, then we write out to it
+    template_file = shmem.data['template_file']
+    if template_file:
+	with open(os.path.basename(template_file), 'w') as out_fobj:
+	    template_subs_file(template_file, out_fobj, d)
   
     # Perform subsitution, prepend 'srun' if necessary.
     work = string.Template(shmem.data['template']).substitute(d)
@@ -101,47 +124,61 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='jsonrun.py - substitute values into a template and run commands.')
     parser.add_argument('--local', dest='local_procs', type=int, help='Run a maximum of N processes in parallel locally.')
     parser.add_argument('--srun', dest='srun_procs', type=int, help='Run a maximum of N processes in parallel on a cluster with slurm.')
-    parser.add_argument('--template', dest='template', required=True, metavar="'template text'",
+    parser.add_argument('--template', dest='template', metavar="'template text'",
                          help='Command-execution template. Must be in single quotes or \
                                $ character pre-pended to $infile must be escaped.')
+    parser.add_argument('--templatefile', dest='template_file', metavar="'template file'",
+                         help='Command-execution template file path.')
     parser.add_argument('--savecmdfile', dest='savecmd_file', help='Name of the file that will contain the command that was executed.')
     parser.add_argument('--logfile', dest='log_file', default='log.txt', help='Name of the file that will contain the command that was executed.')
     parser.add_argument('--dryrun', action='store_true', help='Run in dryrun mode, does not execute commands.')
     parser.add_argument('<json_files>', nargs='*') # Used sys.argv already for this, but could have done a custom type here.
     arguments = parser.parse_args()
 
+    def insufficient_args(complaint):
+        parser.print_help()
+        parser.exit(1, "\n"+complaint+"\n")
+
     # Make sure at least one JSON file was specified.
     if len(json_files) == 0:
-        parser.print_help()
-        parser.exit(1, "\nError: No JSON files were specified.\n")
-
+        insufficient_args("Error: No JSON files were specified.")
  
     if arguments.local_procs is not None and arguments.srun_procs is not None:
-        parser.print_help()
-        parser.exit(1, "\nError: --srun and --local are mutually exclusive.\n")
+        insufficient_args("Error: --srun and --local are mutually exclusive.")
     
-    
-    template = arguments.template
+    # Make sure that either a template or a template file was given
+    if arguments.template:
+	template = arguments.template
+    elif arguments.template_file:
+	template = TEMPLATEFILE_RUN_CMD + arguments.template_file
+    else:
+	insufficient_args("Error: Please specify either a template or a template file")
 
     # Grab max procs if specified and whether or not srun will be used.
     if arguments.local_procs is not None: 
         max_procs = arguments.local_procs
-    elif  arguments.srun_procs is not None:
+    elif arguments.srun_procs is not None:
         max_procs = arguments.srun_procs
         srun = True
     
     if arguments.dryrun is not None:
         dryrun = arguments.dryrun
 
-    return dryrun, template, arguments.savecmd_file, arguments.log_file, srun, max_procs, json_files
+    # NOTE: Brian-- this many positional arguments freaks me out.
+    # is there any reason not to return a dictionary with the correspondig key-value pairs, and then do
+    # shmem.data = parse_arguments()
+    # ??
+
+    return dryrun, template, arguments.template_file, arguments.savecmd_file, arguments.log_file, srun, max_procs, json_files
 
 def main():
-    dryrun, template, savecmd_file, log_file, srun, max_procs, json_files = parse_arguments()
+    dryrun, template, template_file, savecmd_file, log_file, srun, max_procs, json_files = parse_arguments()
     # Create a dictionary that will be shared amongst all forked processes.
     shmem.data['dryrun'] = dryrun
     shmem.data['srun'] = srun
     shmem.data['start_directory'] = os.getcwd()
     shmem.data['template'] = template
+    shmem.data['template_file'] = template_file
     shmem.data['savecmd_file'] = savecmd_file
     shmem.data['log_file'] = log_file
     invoke(max_procs, json_files)
