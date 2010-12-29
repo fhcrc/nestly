e32f88c3af3eeaa5e6e698c515a1b84dc3a3fefd
Brian Hodges <bhodges@fhcrc.org>
2010-12-29 12:36:37 -0800

    removed --dryrun from do_nestrun.sh

--- examples/jsonrun/do_nestrun.sh
+++ examples/jsonrun/do_nestrun.sh
@@ -1,1 +1,1 @@
-nestrun.py --dryrun --savecmdfile cmd.sh --srun 2 --template='raxmlHPC -T 2 -m GTRGAMMA -n bl4 -s $infile' $(find runs -name control.json)
+nestrun.py --savecmdfile cmd.sh --srun 2 --template='raxmlHPC -T 2 -m GTRGAMMA -n bl4 -s $infile' $(find runs -name control.json)
