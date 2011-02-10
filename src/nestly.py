cb11979fbbb64020159be47d9e209703164f9963
Erick Matsen <matsen@fhcrc.org>
2011-02-10 12:54:29 -0800

    moving to spaces rather than tabs

--- src/nestly.py
+++ src/nestly.py
@@ -3,26 +3,26 @@ import json, os, glob, copy, re, collections
 ## internals
 def d_of_jsonfile(fname):
     with open(fname, 'r') as ch:
-	return(json.load(ch))
+        return(json.load(ch))
 
 def d_to_jsonfile(fname, d):
     with open(fname, 'w') as ch:
-	ch.write(json.dumps(d, indent=4)+"\n")
+        ch.write(json.dumps(d, indent=4)+"\n")
 
 def nvd_to_jsonfile(fname, d):
     d_to_jsonfile(fname, 
-	    collections.OrderedDict([(k, v.val) for (k, v) in d.items()]))
+            collections.OrderedDict([(k, v.val) for (k, v) in d.items()]))
 
 def create_dir(dirname):
     """only makes a directory if needed, and complains if it's blocked by a file"""
     if os.path.isfile(dirname):
-	raise IOError("file blocking mkdir: "+dirname)
+        raise IOError("file blocking mkdir: "+dirname)
     if not os.path.isdir(dirname):
-	os.mkdir(dirname)
+        os.mkdir(dirname)
 
 def assert_extant(fname):
     if not(os.path.isfile(fname) or os.path.isdir(fname)):
-	raise IOError("path does not exist: "+fname)
+        raise IOError("path does not exist: "+fname)
 
 def filter_dir(pathl):
     [path for path in pathl if os.path.isdir(path)]
@@ -55,9 +55,9 @@ def path_list_of_pathpair(path, filel):
 def nonempty_glob(g):
     globbed = glob.glob(g)
     if globbed == []:
-	raise IOError("empty glob: "+g)
+        raise IOError("empty glob: "+g)
     else:
-	return(globbed)
+        return(globbed)
 
 def collect_globs(path, globl):
     return(sum([nonempty_glob(g) for g in path_list_of_pathpair(path, globl)], []))
@@ -82,13 +82,13 @@ def mirror_dir(start_path, start_paraml, control):
     """mirror a directory tree"""
     # recur by taking all globs from previous dir
     def aux(paraml):
-	if 1 < len(paraml):
-	    control[paraml[1]] = (
-		lambda(c): map(dir_nv, filter_dir(collect_globs(c[paraml[0]].val, "*"))))
-	    aux(paraml[1:])
+        if 1 < len(paraml):
+            control[paraml[1]] = (
+                lambda(c): map(dir_nv, filter_dir(collect_globs(c[paraml[0]].val, "*"))))
+            aux(paraml[1:])
     # start recursion by doing all directories
     if start_paraml:
-	control[start_paraml[0]] = all_dir_globs(dir_nv, start_path, ["*"])
+        control[start_paraml[0]] = all_dir_globs(dir_nv, start_path, ["*"])
         aux(start_paraml)
 
 # we choose to strip the extension and then replace all slashes with dashes
@@ -99,25 +99,25 @@ def dirname_of_path(path):
 # the actual recursion
 def _aux_build(control, paraml):
     if not paraml:
-	nvd_to_jsonfile("control.json", control)
-	return()
+        nvd_to_jsonfile("control.json", control)
+        return()
     else:
-	# json_to_file("precontrol.json", control)
-	curr = paraml[0]
-	level_control = copy.copy(control)
-	for nv in control[curr](control):
-	    level_control[curr] = nv
-	    if nv.name:
-		# if there is a name then we make a directory
-		# below: we only want to make one directory level at a time
-    	        assert(not (re.search("/", nv.name)))
-    	        create_dir(nv.name)
-    	        os.chdir(nv.name)
-    	        _aux_build(level_control, paraml[1:])
-    	        os.chdir("..")
-	    else:
-		# no name-- just recur through
-		_aux_build(level_control, paraml[1:])
+        # json_to_file("precontrol.json", control)
+        curr = paraml[0]
+        level_control = copy.copy(control)
+        for nv in control[curr](control):
+            level_control[curr] = nv
+            if nv.name:
+                # if there is a name then we make a directory
+                # below: we only want to make one directory level at a time
+                    assert(not (re.search("/", nv.name)))
+                    create_dir(nv.name)
+                    os.chdir(nv.name)
+                    _aux_build(level_control, paraml[1:])
+                    os.chdir("..")
+            else:
+                # no name-- just recur through
+                _aux_build(level_control, paraml[1:])
 
 def build(complete):
     destdir = complete["destdir"]
