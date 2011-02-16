23b1cc9bb73f716ddfe9e51b0a054f67858bff70
Aaron Gallagher <habnabit@gmail.com>
2011-02-15 22:10:42 -0800

    Further code cleanup.
     * Removed nestly.shmem.
     * Removed some redundant code.
     * Using argparse for some more things it should've been used for.

--- nestly/scripts/nestrun.py
+++ nestly/scripts/nestrun.py
@@ -10,10 +10,6 @@ import sys
 import os
 
 from nestly.nestly import *
-from nestly import shmem
-
-# This will get populated in main() and later be shared by all children.
-shmem.data = {}
 
 # Constants to be used as defaults.
 MAX_PROCS = 2                    # Set the default maximum number of child processes that can be spawned.
@@ -25,7 +21,7 @@ DRYRUN = False                   # Run in dryrun mode, default is False.
 TEMPLATEFILE_RUN_CMD = 'source '
 
 
-def invoke(max_procs, json_files):
+def invoke(max_procs, data, json_files):
     procs = {}
     files = iter(json_files)
     while True:
@@ -36,7 +32,7 @@ def invoke(max_procs, json_files):
                 if not procs:
                     return
                 break
-            g = worker(json_file)
+            g = worker(data, json_file)
             try:
                 proc = g.next()
             except StopIteration:
@@ -54,54 +50,49 @@ def invoke(max_procs, json_files):
             raise ValueError('worker generators should only yield once')
 
 
-# NOTE: can we avoid doing this by passing a flag to open to make it barf?
-def assert_file_exists(path):
-    if not os.path.isfile(path):
-	raise IOError("path "+path+" is not a file")
-
 # use d to do template substitution on each line of in_file and write the output to out_fobj
 def template_subs_file(in_file, out_fobj, d):
-    assert_file_exists(in_file)
     with open(in_file, 'r') as in_fobj:
 	for line in in_fobj:
 	    out_fobj.write(Template(line).substitute(d))
 
-def worker(json_file):
+def worker(data, json_file):
     """
     Handle parameter substitution and execute command as child process.
     """
     # PERHAPS TODO: Support either full or relative paths.
-    d = d_of_jsonfile(json_file)
-    json_directory = os.path.dirname(json_file)
+    with json_file:
+        d = json.load(json_file)
+    json_directory = os.path.dirname(json_file.name)
     def p(*parts):
         return os.path.join(json_directory, *parts)
 
     # STDOUT and STDERR will be writtne to a log file in each job directory.
-    log_file = shmem.data['log_file']
+    log_file = data['log_file']
 
     # A template file will be written in each job directory, including the
     # substitution that was performed..
-    savecmd_file = shmem.data['savecmd_file']
+    savecmd_file = data['savecmd_file']
 
     # if a template file is being used, then we write out to it
-    template_file = shmem.data['template_file']
+    template_file = data['template_file']
     if template_file:
 	with open(p(template_file), 'w') as out_fobj:
 	    template_subs_file(template_file, out_fobj, d)
 
     # Perform subsitution, prepend 'srun' if necessary.
-    work = Template(shmem.data['template']).substitute(d)
-    if shmem.data['srun']:
+    work = Template(data['template']).substitute(d)
+    if data['srun']:
         work = SRUN_COMMAND + work
 
     if savecmd_file:
 	with open(p(savecmd_file), 'w') as command_file:
 	    command_file.write(work + "\n")
 
-    print "Execution directory currently: " + os.getcwd()
+    print "Execution directory: %s" % (p(),)
 
     # View what actions will take place in dryrun mode.
-    if shmem.data['dryrun']:
+    if data['dryrun']:
 	print "dry run of: " + work + "\n"
     else:
 	print "running: " + work + "\n"
@@ -118,16 +109,6 @@ def worker(json_file):
             print "Error executing: " + work + "\n"
 
 
-def json_file_test(argument):
-    """
-    Test to make sure the path leads to a .json file that is readable.
-    """
-    # Whitespace in filenames is *not* supported.
-    filename = argument.replace(' ', '')
-    if os.access(filename, os.R_OK) and os.path.isfile(filename) and '.json' in argument.lower():
-        return argument
-
-
 def parse_arguments():
     """
     Grab options and json files.
@@ -136,9 +117,6 @@ def parse_arguments():
     dryrun = DRYRUN
     srun = SRUN
 
-    # We will use argv to build up a list of files.
-    argv = sys.argv[1:]
-    json_files = filter(json_file_test, argv)
     parser = argparse.ArgumentParser(description='jsonrun.py - substitute values into a template and run commands.')
     parser.add_argument('--local', dest='local_procs', type=int, help='Run a maximum of N processes in parallel locally.')
     parser.add_argument('--srun', dest='srun_procs', type=int, help='Run a maximum of N processes in parallel on a cluster with slurm.')
@@ -150,16 +128,12 @@ def parse_arguments():
     parser.add_argument('--savecmdfile', dest='savecmd_file', help='Name of the file that will contain the command that was executed.')
     parser.add_argument('--logfile', dest='log_file', default='log.txt', help='Name of the file that will contain the command that was executed.')
     parser.add_argument('--dryrun', action='store_true', help='Run in dryrun mode, does not execute commands.')
-    parser.add_argument('<json_files>', nargs='*') # Used sys.argv already for this, but could have done a custom type here.
+    parser.add_argument('json_files', type=argparse.FileType('r'), nargs='+')
     arguments = parser.parse_args()
 
     def insufficient_args(complaint):
         parser.print_help()
         parser.exit(1, "\n"+complaint+"\n")
-
-    # Make sure at least one JSON file was specified.
-    if len(json_files) == 0:
-        insufficient_args("Error: No JSON files were specified.")
 
     if arguments.local_procs is not None and arguments.srun_procs is not None:
         insufficient_args("Error: --srun and --local are mutually exclusive.")
@@ -196,8 +170,8 @@ def parse_arguments():
     data['savecmd_file'] = arguments.savecmd_file
     data['log_file'] = arguments.log_file
 
-    return data, max_procs, json_files
+    return data, max_procs, arguments.json_files
 
 def main():
-    shmem.data, max_procs, json_files = parse_arguments()
-    invoke(max_procs, json_files)
+    data, max_procs, json_files = parse_arguments()
+    invoke(max_procs, data, json_files)
--- nestly/shmem.py
+++
@@ -1,2 +0,0 @@
-# Empty file, just a placeholder so we can create a shared dictionary 
-# that will be accessible amongst all child processes.
