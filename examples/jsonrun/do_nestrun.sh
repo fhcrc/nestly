9eb0cabc14ae1bf2fd282dcbea563e4c3c78f9a0
Connor McCoy <connorm@gmail.com>
2011-03-16 13:55:59 -0700

    Switch do_nestrun to use str.format templating

--- examples/jsonrun/do_nestrun.sh
+++ examples/jsonrun/do_nestrun.sh
@@ -1,3 +1,3 @@
-nestrun --savecmdfile cmd.sh --srun 2 --template='raxmlHPC -T 2 -m GTRGAMMA -n bl4 -s $infile' $(find runs -name control.json)
+nestrun --savecmdfile cmd.sh --srun 2 --template='raxmlHPC -T 2 -m GTRGAMMA -n bl4 -s {infile}' $(find runs -name control.json)
 # Non-srun environment, non-thread raxml example
-#nestrun.py --savecmdfile cmd.sh --template='raxmlHPC -m GTRGAMMA -n bl4 -s $infile' $(find runs -name control.json)
+#nestrun.py --savecmdfile cmd.sh --template='raxmlHPC -m GTRGAMMA -n bl4 -s {infile}' $(find runs -name control.json)
