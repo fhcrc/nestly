11583cfca501164c5c08af70f66d430cd180dbc5
Connor McCoy <cmccoy@fhcrc.org>
2011-09-20 17:22:44 -0700

    Update basic_nest for new API

--- examples/basic_nest/make_nest.py
+++ examples/basic_nest/make_nest.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 
-import collections
+import glob
 import os
 import os.path
-import sys
-from nestly import nestly
+from nestly import Nest
 
 wd = os.getcwd()
 input_dir = os.path.join(wd, 'inputs')
 
-ctl = collections.OrderedDict()
+nest = Nest()
+nest.add_level('strategy', ('exhaustive', 'approximate'))
+nest.add_level('run_count', [10**i for i in xrange(3)])
+nest.add_level('input_file', glob.glob(os.path.join(input_dir, 'file*')),
+        label_func=os.path.basename)
 
-ctl['strategy'] = nestly.repeat_iterable(('exhaustive', 'approximate'))
-ctl['run_count'] = nestly.repeat_iterable([10**(i + 1) for i in xrange(3)])
-ctl['input_file'] = lambda x: map(nestly.file_nv, nestly.collect_globs(input_dir, ['file*']))
-
-nestly.build(ctl, 'runs')
+nest.build('runs')
