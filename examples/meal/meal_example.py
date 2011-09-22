6c86f591a0c1684f1abc7b6185c1178355f6b367
Connor McCoy <cmccoy@fhcrc.org>
2011-09-21 17:28:31 -0700

    Update meal_example
    this one is a little nastier since the name/val combination isn't
    available, but I don't find myself using these functions much in real
    use.

--- examples/meal/meal_example.py
+++ examples/meal/meal_example.py
@@ -1,31 +1,45 @@
-#######!/usr/bin/env python
+#!/usr/bin/env python
 
-import sys, os, collections, json
-from nestly.nestly import *
+import collections
+import json
+import glob
+import os
+import os.path
+import sys
+
+from nestly import Nest
 
 wd = os.getcwd()
-startersdir = os.path.join(wd,"starters/")
-winedir = os.path.join(wd,"wine/")
-mainsdir = os.path.join(wd,"mains/")
+startersdir = os.path.join(wd, "starters")
+winedir = os.path.join(wd, "wine")
+mainsdir = os.path.join(wd, "mains")
 
-ctl = collections.OrderedDict({})
+nest = Nest()
+
+bn = os.path.basename
+def strip_bn(path):
+    """Return the basename of path, any extension stripped"""
+    return bn(os.path.splitext(path)[0])
 
 # start by mirroring the two directory levels in startersdir, and name those
 # directories "ethnicity" and "dietary"
-mirror_dir(startersdir, ["ethnicity", "dietary"], ctl)
+nest.add('ethnicity', glob.glob(os.path.join(startersdir, '*')),
+    label_func=bn)
+nest.add('dietary', lambda c: glob.glob(os.path.join(c['ethnicity'], '*')),
+    label_func=bn)
 
-# now get all of the starters
-ctl["starter"] = (lambda(c): map(file_nv, collect_globs(c["dietary"].val, ["*"])))
+## now get all of the starters
+nest.add('starter', lambda c: glob.glob(os.path.join(c['dietary'], '*')),
+    label_func=strip_bn)
+## now get the corresponding mains
+nest.add('main', lambda c: [os.path.join(mainsdir, bn(c['ethnicity']) + "_stirfry.txt")],
+    label_func=strip_bn)
 
-# now get the corresponding mains
-ctl["main"] = (lambda(c): [file_nv(mainsdir+c["ethnicity"].name+"_stirfry.txt")])
+## get only the tasty wines
+nest.add('wine', glob.glob(os.path.join(winedir, '*.tasty')),
+    label_func=strip_bn)
+## the wineglasses should be chosen by the wine choice, but we don't want to
+## make a directory for those.
+nest.add('wineglass', lambda c: c['wine'] + ' wine glasses', create_dir=False)
 
-# get only the tasty wines
-ctl["wine"] = all_globs(file_nv, winedir, ["*.tasty"])
-
-# the wineglasses should be chosen by the wine choice, but we don't want to
-# make a directory for those, so they get a none_nv
-ctl["wineglass"] = (lambda(c): [none_nv(c["wine"].name+" wine glasses")])
-
-build(ctl, "runs")
-
+nest.build('runs')
