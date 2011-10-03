7d31d8ed9834d14247d6f3ef246cdd21fa5ccbdc
Connor McCoy <cmccoy@fhcrc.org>
2011-10-03 11:56:11 -0700

    Initial work on nestagg

---
+++ nestly/scripts/nestagg.py
@@ -0,0 +1,76 @@
+"""
+Aggregate results of ``nestly`` runs.
+"""
+
+import argparse
+import collections
+import csv
+import functools
+import itertools
+import os.path
+import json
+import sys
+
+DEFAULT_SEP = ','
+
+# JSON loaders returning OrderedDicts
+_ordered_load = functools.partial(json.load,
+                                  object_pairs_hook=collections.OrderedDict)
+_ordered_loads = functools.partial(json.loads,
+                                   object_pairs_hook=collections.OrderedDict)
+
+def _delim_accum(delimited_files, keys=None, separator=DEFAULT_SEP):
+    """
+    Accumulator for delimited files - combine each file
+    """
+    for f in delimited_files:
+        dn = os.path.dirname(f)
+        with open(os.path.join(dn, 'control.json')) as fp:
+            control = _ordered_load(fp)
+
+        keys = frozenset(keys or control.keys())
+
+        with open(f) as fp:
+            reader = csv.DictReader(fp, delimiter=separator)
+            for row in reader:
+                row_dict = collections.OrderedDict(itertools.chain(
+                        ((k, row[k]) for k in reader.fieldnames),
+                        ((k, v) for k, v in control.items() if k in keys))
+                )
+
+                yield row_dict
+
+def delim(arguments):
+    """
+    Execute delim action.
+    """
+    with arguments.output as fp:
+        results = _delim_accum(arguments.delimited_files, arguments.keys,
+                               arguments.separator)
+        r = next(results)
+        writer = csv.DictWriter(fp, r.keys(), delimiter=arguments.separator)
+        writer.writeheader()
+        writer.writerow(r)
+        writer.writerows(results)
+
+def main(args=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description="""Accumulate results of
+            nestly runs""")
+    subparsers = parser.add_subparsers()
+    delim_parser = subparsers.add_parser('delim', help="""Combine control files
+            with delimited files.""")
+    delim_parser.set_defaults(func=delim)
+    delim_parser.add_argument('-k', '--keys', help="""Keys to include [default:
+            all]""")
+    #delim_parser.add_argument('-d', '--directory',
+        #help="""Directory to search for control.json files""")
+    delim_parser.add_argument('delimited_files', metavar="delim_file",
+            nargs="+")
+    delim_parser.add_argument('-s', '--separator', default=DEFAULT_SEP,
+            help="""Separator [default: %(default)s]""")
+    delim_parser.add_argument('-o', '--output', default=sys.stdout,
+        type=argparse.FileType('w'), help="""Output file [default: stdout]""")
+
+    arguments = parser.parse_args()
+
+    arguments.func(arguments)
