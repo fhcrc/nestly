5ed10713e2586cb27ef11c9b15c0c01f8dc9f413
Connor McCoy <cmccoy@fhcrc.org>
2012-05-17 09:14:29 -0700

    Use stripext from main library in meal ex.

--- examples/meal/meal_example.py
+++ examples/meal/meal_example.py
@@ -4,7 +4,7 @@ import glob
 import os
 import os.path
 
-from nestly import Nest
+from nestly import Nest, stripext
 
 wd = os.getcwd()
 startersdir = os.path.join(wd, "starters")
@@ -14,9 +14,6 @@ mainsdir = os.path.join(wd, "mains")
 nest = Nest()
 
 bn = os.path.basename
-def strip_bn(path):
-    """Return the basename of path, any extension stripped"""
-    return bn(os.path.splitext(path)[0])
 
 # start by mirroring the two directory levels in startersdir, and name those
 # directories "ethnicity" and "dietary"
@@ -27,17 +24,17 @@ nest.add('dietary', lambda c: glob.glob(os.path.join(c['ethnicity'], '*')),
 
 ## now get all of the starters
 nest.add('starter', lambda c: glob.glob(os.path.join(c['dietary'], '*')),
-    label_func=strip_bn)
+    label_func=stripext)
 ## now get the corresponding mains
 nest.add('main', lambda c: [os.path.join(mainsdir, bn(c['ethnicity']) + "_stirfry.txt")],
-    label_func=strip_bn)
+    label_func=stripext)
 
 ## get only the tasty wines
 nest.add('wine', glob.glob(os.path.join(winedir, '*.tasty')),
-    label_func=strip_bn)
+    label_func=stripext)
 ## the wineglasses should be chosen by the wine choice, but we don't want to
 ## make a directory for those.
-nest.add('wineglass', lambda c: [strip_bn(c['wine']) + ' wine glasses'],
+nest.add('wineglass', lambda c: [stripext(c['wine']) + ' wine glasses'],
         create_dir=False)
 
 nest.build('runs')
