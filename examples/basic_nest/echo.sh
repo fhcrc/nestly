ef45fdb0d6879d0a3432fe8eecb619bee57cb2b8
Erick Matsen <matsen@fhcrc.org>
2012-05-22 10:23:04 -0700

    work on examples

--- examples/basic_nest/echo.sh
+++ examples/basic_nest/echo.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/bin/sh
+
 echo "var1,var2
 13,{run_count}0"
--- examples/jsonrun/do_nestrun.sh
+++
@@ -1,1 +0,0 @@
-nestrun --save-cmd-file cmd.sh -j 2 --template='raxmlHPC -T 2 -m GTRGAMMA -n bl4 -s {infile}' $(find runs -name control.json)
