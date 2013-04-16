75bfe2107e38c4f64282e58da30a59f20211bcf5
Connor McCoy <cmccoy@fhcrc.org>
2013-04-15 17:27:00 -0700

    Release 0.4.0

--- nestly/__init__.py
+++ nestly/__init__.py
@@ -3,6 +3,6 @@ nestly is a collection of functions designed to make running software with
 combinatorial choices of parameters easier.
 """
 
-__version__ = '0.3'
+__version__ = '0.4'
 
 from .core import Nest, nest_map, stripext
