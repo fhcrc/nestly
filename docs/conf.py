8a734011b7acac9fd45ed9b79db12402a2caa297
Connor McCoy <cmccoy@fhcrc.org>
2014-04-11 09:56:52 -0700

    Release v0.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'nestly'
-copyright = u'2011, Erick Matsen et al.'
+copyright = u'2011-2014, Erick Matsen et al.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
