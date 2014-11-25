b04e3a6d19113e9d54df867926d0877f6b49863d
Chris Warth <cwarth@fhcrc.org>
2014-11-25 15:14:50 -0800

    release 0.6 via travis

--- nestly/__init__.py
+++ nestly/__init__.py
@@ -3,6 +3,6 @@ nestly is a collection of functions designed to make running software with
 combinatorial choices of parameters easier.
 """
 
-__version__ = '0.6-dev'
+__version__ = '0.6'
 
 from .core import Nest, nest_map, stripext
